27e6c25abe349e8243e0c1ec20b44144a186f239
GioRigamonti <76151712+GioRigamonti@users.noreply.github.com>
2021-04-08 19:26:05 +0200

    aggiunto zip

--- ADL_Model.py
+++ ADL_Model.py
@@ -3,13 +3,26 @@
 @author: Jessica Maggioni 845389
 @author: Giorgia Rigamonti 844619
 """
-
+import zipfile
 import json
-file_path= './MOTIONSENSE.json'
+
+
+file_name = 'MOTIONSENSE'
+#apertura dell'archivio per estrazione
+with zipfile.ZipFile(file_name + '.zip') as myzip:
+    with myzip.open(file_name + '.json') as myfile:
+        #lettura e conversione da bytecode a str e eliminazione degli spazi(/r/n)
+        r = myfile.read().decode('utf-8').strip() 
+            
+
+
 fieldsToDelete = ['db_id','type','sensors','config_id']
 
-with open(file_path) as f:
-    r = f.read() #estrae la stringa del file .json
+
+#alternativa senza zip
+#with open(file_path) as f:
+ #   r = f.read() #estrae la stringa del file .json
+
 
 #crea una lista di dizionari, uno per ogni elemento della stringa letta dal file .json
 data = json.loads(r) #type(data) = list
@@ -20,3 +33,5 @@ for data_item in data:
         del data_item[i] 
 
 #accesso ad una sola componente del dizionario: data[indice][dictKey]
+print(len(data[20]['accelerometer']))
+myzip.close()
