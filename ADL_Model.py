4f7feb6e989c72d30b4b72fb692f1cb40cceef0f
j-maggioni <jessicamaggioni99@gmail.com>
2021-04-09 09:39:34 +0200

    eliminato gyroscope da dizionario

--- ADL_Model.py
+++ ADL_Model.py
@@ -11,12 +11,12 @@ file_name = 'MOTIONSENSE'
 #apertura dell'archivio per estrazione
 with zipfile.ZipFile(file_name + '.zip') as myzip:
     with myzip.open(file_name + '.json') as myfile:
-        #lettura e conversione da bytecode a str e eliminazione degli spazi(/r/n)
+        #lettura e conversione da bytecode a str e eliminazione degli spazi(\r\n)
         r = myfile.read().decode('utf-8').strip() 
             
 
 
-fieldsToDelete = ['db_id','type','sensors','config_id']
+fieldsToDelete = ['db_id','type','sensors','config_id', 'gyroscope']
 
 
 #alternativa senza zip
@@ -27,11 +27,13 @@ fieldsToDelete = ['db_id','type','sensors','config_id']
 #crea una lista di dizionari, uno per ogni elemento della stringa letta dal file .json
 data = json.loads(r) #type(data) = list
 
-#elimina i campi inutilizzati, mantiene solo _id, label, accelerometer, gyroscope
+#elimina i campi inutilizzati, mantiene solo _id, label, accelerometer 
 for data_item in data: 
     for i in fieldsToDelete:
         del data_item[i] 
 
+
 #accesso ad una sola componente del dizionario: data[indice][dictKey]
-print(len(data[20]['accelerometer']))
+
+#chiusura del file .zip
 myzip.close()
