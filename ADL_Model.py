bd18d1220b50c4c430a06827b1eb968445b9398b
GioRigamonti <76151712+GioRigamonti@users.noreply.github.com>
2021-04-12 19:48:53 +0200

    Update ADL_Model.py

--- ADL_Model.py
+++ ADL_Model.py
@@ -5,15 +5,15 @@
 """
 import zipfile
 import json
-import csv
 import tensorflow as tf
-import numpy as np
+#import numpy as np
 import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
 from tensorflow.keras.layers.experimental.preprocessing import Normalization
 from tensorflow.keras.layers.experimental.preprocessing import CategoryEncoding
-from tensorflow.keras.layers.experimental.preprocessing import StringLookup
+
      
 
 file_name = 'MOTIONSENSE'
@@ -65,7 +65,7 @@ for data_dict in data:
 
 """   
 #DICT_TO_DATAFRAME giusto
-a = data[0]
+
 
 acc_x = []
 acc_y = []
@@ -102,28 +102,8 @@ df.insert(loc = indx+1, column = 'id', value = id_list)
 
 
 
-
-
-
-
-"""
 #accesso ad una sola componente del dizionario: data[indice][dictKey]
-#conversione dei dati in un file.csv
-for item_data in data:
-    # creazione dell'intestazione del file .csv
-    item = ['accelerometer x ', 'accelerometer y ', 'accelerometer z ','activity']
-    #apertura di un file in scrittura per il caricamento dei dati
-    data_file = open(item_data['_id']['$oid'] + '.csv', 'w')
-    with data_file:
-        # creazione del csv writer object 
-        csv_writer = csv.writer(data_file, dialect = 'excel')
-        #scrittura dell'intestazione del file .cvs
-        csv_writer.writerow(item)
-        #riempimento del file .csv con tutti i dati relativi all'accelerometro
-        for item in item_data['accelerometer']:
-            header = item + [item_data['label']]               
-            csv_writer.writerow(header)
-"""
+
 """
 #alternativa per un unico file csv con tutti i dati
 # creazione dell'intestazione del file .csv
@@ -141,70 +121,31 @@ with data_file:
             header = item + [item_data['label']]              
             csv_writer.writerow(header)
 """
-"""
-versione 1
-#zip_name = 'MOTIONSENSE.zip'
-#apertura dell'archivio per estrazione
-#with zipfile.ZipFile(zip_name) as myzip:
-#   file_name = myzip.namelist()[0]
-#   ms = pd.read_json(file_name)
- #  ms.head()
-
-#df = pd.DataFrame(ms.data, columns = ms.feature_names)
-# Convert datatype to float
-#df = df.astype(float)
-# append "target" and name it "label"
-#df['label'] = ms.target
-# Use string label instead
-#df['label'] = df.label.replace(dict(enumerate(ms.target_names)))
-# label -> one-hot encoding
-#label = pd.get_dummies(df['label']) #concatena 'label' al nome delle attività
-#label.columns = ['label_' + str(x) for x in label.columns]
-#df = pd.concat([df, label], axis=1)
-# drop old label
-#df.drop(['label'], axis=1, inplace=True)
-
-# Creating X and y
-#X = df[['accelerometer x ', 'accelerometer y ', 'accelerometer z ']]
-# Convert DataFrame into np array
-#X = np.asarray(X)
-#y = df[['stairs down ', ' jogging ', 'sitting ', 'standing ', 'stairs up ', 'walking ']]
-# Convert DataFrame into np array
-#y = np.asarray(y)
-
-#X_train, X_test, y_train, y_test = train_test_split(
- # X,
-  #y,
-  #test_size = None
-#)
-
-#print (len(X_train))
-#print (len(X_test))
-#print (len(y_train))
-#print (len(y_test))
-"""
 
 
 
 
-"""
-#file_url = '5fbaa22cab33096d8049d73c.csv' #file_name + '.csv'
-file_url = 'MOTIONSENSE.csv'
-dataframe = pd.read_csv(file_url)
+
+
+
+dataframe = df
+
 
 dataframe.shape
 dataframe.head()
-val_dataframe = dataframe.sample(replace = False)
+val_dataframe = dataframe.sample(replace = False, random_state = 1337, axis = 0)
+
 train_dataframe = dataframe.drop(val_dataframe.index)
 
-print(
-    "Using %d samples for training and %d for validation"
+
+print("Using %d samples for training and %d for validation"
     % (len(train_dataframe), len(val_dataframe)))
 
 
 def dataframe_to_dataset(dataframe):
     dataframe = dataframe.copy()
-    labels = dataframe.pop('activity')
+    labels = dataframe.pop('label')
+    dataframe.pop('id')
     ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
     ds = ds.shuffle(buffer_size=len(dataframe))
     return ds
@@ -212,11 +153,13 @@ def dataframe_to_dataset(dataframe):
 train_ds = dataframe_to_dataset(train_dataframe)
 val_ds = dataframe_to_dataset(val_dataframe)
 
-for x, y in train_ds.take(2095):
+for x, y in train_ds.take(2):
     print("Input:\n", x)
     print()
     print("Activity:\n", y)
     print()
+
+
 
 train_ds = train_ds.batch(32)
 val_ds = val_ds.batch(32)
@@ -236,7 +179,7 @@ def encode_numerical_feature(feature, name, dataset):
     # Normalize the input feature
     encoded_feature = normalizer(feature)
     return encoded_feature
-"""
+
 
 """
 def encode_string_categorical_feature(feature, name, dataset):
@@ -265,17 +208,18 @@ def encode_string_categorical_feature(feature, name, dataset):
 # Apply one-hot encoding to our indices
     encoded_feature = encoder(encoded_feature)
     return encoded_feature
+
 """
-"""
+
 #Categorical features encoded as float64
 
-acc_x = keras.Input(shape=(1,), name = 'accelerometer x ', dtype = 'float64')
-acc_y = keras.Input(shape=(1,), name = 'accelerometer y ', dtype = 'float64')
-acc_z = keras.Input(shape=(1,), name = 'accelerometer z ', dtype = 'float64')
+acc_x = keras.Input(shape=(1,), name = 'acc_x', dtype = 'float64')
+acc_y = keras.Input(shape=(1,), name = 'acc_y', dtype = 'float64')
+acc_z = keras.Input(shape=(1,), name = 'acc_z', dtype = 'float64')
 
 
 # Categorical feature encoded as string
-activity = keras.Input(shape=(1,), name = 'activity', dtype = 'string')
+activity = keras.Input(shape=(1,), name = 'label', dtype = 'string')
 
 
 
@@ -287,9 +231,9 @@ all_inputs = [
 ]
 
 # Integer categorical features
-acc_x_encoded = encode_numerical_feature(acc_x, 'accelerometer x ', train_ds)
-acc_y_encoded = encode_numerical_feature(acc_y, 'accelerometer y ', train_ds)
-acc_z_encoded = encode_numerical_feature(acc_z, 'accelerometer z ', train_ds)
+acc_x_encoded = encode_numerical_feature(acc_x, 'acc_x', train_ds)
+acc_y_encoded = encode_numerical_feature(acc_y, 'acc_y', train_ds)
+acc_z_encoded = encode_numerical_feature(acc_z, 'acc_z', train_ds)
 
 
 
@@ -303,19 +247,21 @@ all_features = layers.concatenate(
         #activity_encode
     ]
 )
-"""
-"""
-x = layers.Dense(32, activation = 'relu')(all_features) 
+
+x = layers.Dense(32, activation = 'sigmoid')(all_features)
+
 x = layers.Dropout(0.5)(x)
+
 output = layers.Dense(6, activation = 'sigmoid')(x)
+
 model = keras.Model(all_inputs, output)
-model.compile(optimizer = 'SGD', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-keras.utils.plot_model(model, show_shapes = True, show_dtype = True,
-                        show_layer_names = True, rankdir = 'LR')
-"""
-#model.fit(train_ds, epochs=50, validation_data=val_ds)
+model.compile('SGD', 'categorical_crossentropy', metrics = ["accuracy"])
 
+#keras.utils.plot_model(model, show_shapes = True, rankdir = 'LR')
+
+model.save('./model.tf')
+#model.fit(train_ds, epochs=50, validation_data = val_ds)
 
 
 
