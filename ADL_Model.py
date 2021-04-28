f5afce690e642fd7a35c84ba32257a7162962da9
j-maggioni <jessicamaggioni99@gmail.com>
2021-04-28 12:13:58 +0200

    ADL_Model con Converter

--- ADL_Model.py
+++ ADL_Model.py
@@ -5,14 +5,10 @@
 """
 import zipfile
 import json
-#import tensorflow as tf
+import tensorflow as tf
 import pandas as pd
 import numpy as np
-from tensorflow.keras.layers import Dense, Dropout 
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPool2D
-from tensorflow.keras.layers import Flatten
-#from tensorflow.keras.layers import *
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.models import Sequential
 from sklearn.model_selection import train_test_split
 
@@ -56,24 +52,6 @@ dict_acc={'acc_x': acc_x_list, 'acc_y': acc_y_list, 'acc_z': acc_z_list,
 # creazione di un dataset da dizionario
 dataset = pd.DataFrame.from_dict(dict_acc)
 
-"""
-#alternativa per un unico file csv con tutti i dati
-# creazione dell'intestazione del file .csv
-item = ['accelerometer x ', 'accelerometer y ', 'accelerometer z ','activity']
-#apertura di un file in scrittura per il caricamento dei dati
-data_file = open(file_name + '.csv', 'w')
-with data_file:
-# creazione del csv writer object (in alternativa aggiungere delimiter = '\t')
-    csv_writer = csv.writer(data_file, dialect = 'excel') 
-    #scrittura dell'intestazione del file .cvs
-    csv_writer.writerow(item)
-    #riempimento del file .csv con tutti i dati relativi all'accelerometro
-    for item_data in data:
-        for item in item_data['accelerometer']:
-            header = item + [item_data['label']]              
-            csv_writer.writerow(header)
-"""
-
 # convalida e addestramento del set di dati
 label = pd.get_dummies(dataset['label'])
 label.columns = ['label_' + str(x) for x in label.columns]
@@ -105,25 +83,19 @@ model.add(Dropout(0.5))
 model.add(Dense(128, activation = 'relu'))
 model.add(Dense(6, activation = 'softmax'))
 model.summary()
+
 #configurazione modello per addestramento
 model.compile(loss='categorical_crossentropy', optimizer='adam', 
               metrics=['accuracy'])
+
 #addestramento del modello
-model.fit(X_train, y_train, validation_split = 1 - 0.8, epochs = 3, 
+model.fit(X_train, y_train, validation_split = 1 - 0.8, epochs = 10, 
           batch_size = 64, verbose = 2, validation_data = (X_test, y_test))
 
+# Convert the model
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Save the model
+with open('ADL_Model.tflite', 'wb') as f:
+  f.write(tflite_model)
