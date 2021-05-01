d4458cd88340841e1699dcda6c6c21136dffa579
GioRigamonti <76151712+GioRigamonti@users.noreply.github.com>
2021-05-01 09:39:15 +0200

    aggiunti campi Profile, Options

--- MyFITapp/app/src/main/java/it/unimib/myfitapp/MainActivity.java
+++ MyFITapp/app/src/main/java/it/unimib/myfitapp/MainActivity.java
@@ -30,12 +30,5 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
 
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.activity_bar_menu,menu);
-        return true;
-    }
-
+    
 }
