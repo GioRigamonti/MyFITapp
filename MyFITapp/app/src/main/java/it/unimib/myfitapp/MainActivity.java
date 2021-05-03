f89e34cada29a61a7d656ceac3575d15f96920d2
j-maggioni <jessicamaggioni99@gmail.com>
2021-05-03 12:54:43 +0200

    add Profile scrollbar

--- MyFITapp/app/src/main/java/it/unimib/myfitapp/MainActivity.java
+++ MyFITapp/app/src/main/java/it/unimib/myfitapp/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         {
             case R.id.action_profile:
                 // definisco l'intenzione di aprire l'Activity "Page1.java"
-                Intent openPage1 = new Intent(MainActivity.this, Registration.class);
+                Intent openPage1 = new Intent(MainActivity.this, Profile.class);
                 // passo all'attivazione dell'activity page1.java
                 startActivity(openPage1);
                 break;
