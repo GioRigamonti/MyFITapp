7a6a1a787eb551e7d3575ee9fc407ada37ce52a0
j-maggioni <jessicamaggioni99@gmail.com>
2021-05-25 11:22:58 +0200

    Update ADLListener.java

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLListener.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLListener.java
@@ -1,4 +1,20 @@
 package it.unimib.adl_library;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+
 public class ADLListener {
+    private int frequenza;
+
+    public void ADLListener(int frequenza){}
+
+    public void onSensorChanged(SensorEvent event){}
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy){}
+
+    public void setFrequenzaDiCampionamento(int millisecondi){}
+
+    public int getFrequenzaDiCampionamento(){
+        return frequenza;
+    }
 }
