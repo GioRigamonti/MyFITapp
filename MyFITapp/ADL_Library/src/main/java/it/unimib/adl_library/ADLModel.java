93013d80cae66dfae44dd6f77d3940cf3c70f8c8
GioRigamonti <76151712+GioRigamonti@users.noreply.github.com>
2021-05-25 11:18:54 +0200

    inserite classi della libreria

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLModel.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLModel.java
@@ -1,7 +1,13 @@
 package it.unimib.adl_library;
 
 public class ADLModel {
+    private ADLModel model;
+    //create constructor
     public void ADLModel() {
+    }
+    // get model method
+    public ADLModel getModel(){
+        return model;
     }
 
 }
---
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/TFRecognizer.java
@@ -0,0 +1,10 @@
+package it.unimib.adl_library;
+
+public class TFRecognizer extends ADLManager {
+    private ADLModel model;
+
+    public TFRecognizer(ADLModel model) {
+        this.model = model;
+    }
+
+}
