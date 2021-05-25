4e059090ed38fd873d9839c6606a324db8530210
j-maggioni <jessicamaggioni99@gmail.com>
2021-05-25 11:14:15 +0200

    Update ADLInstance.java

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLInstance.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLInstance.java
@@ -1,4 +1,34 @@
 package it.unimib.adl_library;
 
 public class ADLInstance {
+    private String activity;
+    private float acc_x, acc_y, acc_z;
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public float getAcc_x() {
+        return acc_x;
+    }
+
+    public void setAcc_x(float acc_x) {
+        this.acc_x = acc_x;
+    }
+
+    public float getAcc_y() {
+        return acc_y;
+    }
+
+    public void setAcc_y(float acc_y) {
+        this.acc_y = acc_y;
+    }
+
+    public float getAcc_z() {
+        return acc_z;
+    }
+
+    public void setAcc_z(float acc_z) {
+        this.acc_z = acc_z;
+    }
 }
