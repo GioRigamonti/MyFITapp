610b173b89262febc8d159b0be5579367d358c26
GioRigamonti <76151712+GioRigamonti@users.noreply.github.com>
2021-05-25 11:23:24 +0200

    inseriti i metodi

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/TFRecognizer.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/TFRecognizer.java
@@ -2,9 +2,30 @@ package it.unimib.adl_library;
 
 public class TFRecognizer extends ADLManager {
     private ADLModel model;
+    private ADLObserver accObserver;
 
     public TFRecognizer(ADLModel model) {
         this.model = model;
     }
+    public boolean initObserverRegistration(ADLObserver observer){
+
+    }
+    public boolean stopObserverRegistration(ADLObserver observer){
+
+    }
+    public void startReadingAccelerometer(){
+
+    }
+    public void stopReadingAccelerometer(){
+
+    }
+    public String getLabel(){
+
+    }
+    public float getConfidence(){
+
+    }
+
+
 
 }
