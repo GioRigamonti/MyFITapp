a0ef5fd143dee57aec2befcbd202364e583a7430
j-maggioni <jessicamaggioni99@gmail.com>
2021-05-25 11:18:15 +0200

    Update ADLObserver.java

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLObserver.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLObserver.java
@@ -1,4 +1,11 @@
 package it.unimib.adl_library;
 
 public interface ADLObserver {
+    private ADLInstance instance;
+
+    public void onNewInstance(ADLInstance instance);
+
+    public String activityIndentified();
+
+    public float[] activityConfidence();
 }
