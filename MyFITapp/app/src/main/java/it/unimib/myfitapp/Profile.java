e90063507c3160cef28d195533d0c8aa26286ed7
GioRigamonti <76151712+GioRigamonti@users.noreply.github.com>
2021-05-28 12:13:13 +0200

    update

--- MyFITapp/app/src/main/java/it/unimib/myfitapp/Profile.java
+++ MyFITapp/app/src/main/java/it/unimib/myfitapp/Profile.java
@@ -142,26 +142,19 @@ public class Profile extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.edit_profile:
-                // definisco l'intenzione di aprire l'Activity "Page1.java"
-                Intent openPage1 = new Intent(Profile.this, EditProfile.class);
-                // passo all'attivazione dell'activity page1.java
-                startActivity(openPage1);
-                break;
             case R.id.action_options:
                 // definisco l'intenzione di aprire l'Activity "Page2.java"
-                Intent openPage2 = new Intent(Profile.this, Settings.class);
+                Intent openPage1 = new Intent(Profile.this, Settings.class);
                 // passo all'attivazione dell'activity page2.java
-                startActivity(openPage2);
+                startActivity(openPage1);
                 break;
-
 
             case android.R.id.home:
                 //finish();
                 //return true;
-                Intent openPage3 = new Intent(Profile.this, MainActivity.class);
+                Intent openPage2 = new Intent(Profile.this, MainActivity.class);
                 // passo all'attivazione dell'activity page2.java
-                startActivity(openPage3);
+                startActivity(openPage2);
                 break;
 
         }
@@ -171,7 +164,7 @@ public class Profile extends AppCompatActivity {
 
     public void navigateLogOut(View v){
         FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
 
