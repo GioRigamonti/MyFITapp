11c81c3adf8d52136a3a1b98b0cdf05446288413
j-maggioni <jessicamaggioni99@gmail.com>
2021-05-31 18:50:21 +0200

    Update ADLManager.java

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLManager.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLManager.java
@@ -1,8 +1,23 @@
 package it.unimib.adl_library;
 
+import android.content.Context;
+import android.hardware.SensorManager;
+
 public abstract class ADLManager {
+    public static final int DEFAULT_SAMPLING_DELAY = 5000;
+
+    private Context mContext = null;
+    protected ADLModel mModelManager = null;
+    protected SensorManager mSensorManager = null;
+    protected long samplingDelay = DEFAULT_SAMPLING_DELAY;
+
     //generate constructor
-    public ADLManager() {
+    public ADLManager(Context context, ADLModel modelManager, long delay) {
+        mContext = context;
+        mModelManager = modelManager;
+        mSensorManager = (SensorManager) mContext
+                .getSystemService(Context.SENSOR_SERVICE);
+        samplingDelay = delay;
     }
     public abstract boolean initObserverRegistration(ADLObserver observer);
     public abstract boolean stopObserverRegistration(ADLObserver observer);
