7901f374ac3748838f8e2cbeb9790dd2cd4b0e85
GioRigamonti <76151712+GioRigamonti@users.noreply.github.com>
2021-06-04 10:09:10 +0200

    sistemato splash screen

--- MyFITapp/app/src/main/java/it/unimib/myfitapp/Splash_Screen.java
+++ MyFITapp/app/src/main/java/it/unimib/myfitapp/Splash_Screen.java
@@ -3,7 +3,6 @@ package it.unimib.myfitapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Home extends AppCompatActivity {
+public class Splash_Screen extends AppCompatActivity {
     private final int DISPLAY_LENGTH = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_splash_screen);
         //Check current user
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         firebaseAuth.addAuthStateListener(authStateListener);
@@ -32,12 +31,12 @@ public class Home extends AppCompatActivity {
             FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
             if (firebaseUser == null) {
-                Intent intent = new Intent(Home.this, Login.class);
+                Intent intent = new Intent(Splash_Screen.this, Login.class);
                 startActivity(intent);
                 finish();
             }
             if (firebaseUser != null) {
-                Intent intent = new Intent(Home.this, MainActivity.class);
+                Intent intent = new Intent(Splash_Screen.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
