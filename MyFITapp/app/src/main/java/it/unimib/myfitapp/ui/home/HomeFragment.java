a18d0bbea34498b6806c96900dc63cd3d44318c3
j-maggioni <jessicamaggioni99@gmail.com>
2021-06-04 10:48:45 +0200

    update fragmentHome

--- MyFITapp/app/src/main/java/it/unimib/myfitapp/ui/home/HomeFragment.java
+++ MyFITapp/app/src/main/java/it/unimib/myfitapp/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,19 +18,24 @@ import it.unimib.myfitapp.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private int progr = 0;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_home);
+
+                
+        final TextView textView = root.findViewById(R.id.text_view_progress_num);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });*/
+        });
+
         return root;
     }
+
 }
