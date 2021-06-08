04404f16e8e7ca570406b39c96e456834182faeb
j-maggioni <jessicamaggioni99@gmail.com>
2021-06-08 10:17:57 +0200

    Update ADLModel.java

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLModel.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLModel.java
@@ -2,33 +2,31 @@ package it.unimib.adl_library;
 
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
-import android.util.Log;
-
 
 import org.tensorflow.lite.Interpreter;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class ADLModel {
-    private ADLModel model;
-    private Interpreter tflite;
     private Context context;
     private final static String MODEL_FILE = "ADL_Model.tflite";
-    //create constructor
+    private final static String LABEL_FILE = "labels.txt";
+
     public MappedByteBuffer ADLModel(Context context) throws IOException {
         this.context = context;
         return loadModelFile(context);
     }
 
-    public String getModelPath() {
+    public static String getModelPath() {
         return MODEL_FILE;
     }
 
+    public static String getLabelPath() {
+        return LABEL_FILE;
+    }
 
     private MappedByteBuffer loadModelFile(Context context)throws IOException{
         //open the model using an input stream and memory map it load
