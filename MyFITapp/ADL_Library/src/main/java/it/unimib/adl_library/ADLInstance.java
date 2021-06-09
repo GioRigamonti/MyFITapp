59b6f8a75bc4eb2c9e9fa362434a9a8597166e82
j-maggioni <jessicamaggioni99@gmail.com>
2021-06-09 12:28:51 +0200

    Update ADLInstance.java

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLInstance.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLInstance.java
@@ -1,45 +1,26 @@
 package it.unimib.adl_library;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ADLInstance {
     private String activity;
-    private static List<Float> acc_x, acc_y, acc_z;
+    private Float acc_x, acc_y, acc_z;
+    List<float[]> acc_Features;
     private long timestamp;
-
 
     public void setActivity(String activity) {
         this.activity = activity;
     }
 
-    public List<Float> getAcc_x() {
-        return acc_x;
+    public void setAcc_tot(float ax, float ay, float az){
+        float[] acc = new float[]{ax, ay, az};
+
+        acc_Features.add(acc);
     }
 
-    public void setAcc_x(float ax) {
-        this.acc_x.add(ax);
-    }
-
-    public List<Float> getAcc_y() {
-        return acc_y;
-    }
-
-    public void setAcc_y(float ay) {
-        this.acc_y.add(ay);
-    }
-
-    public List<Float> getAcc_z() {
-        return acc_z;
-    }
-
-    public void setAcc_z(float az) {
-        this.acc_z.add(az);
-    }
-
-    public void setAccFeatures(float x, float y, float z){
-        setAcc_x(x);
-        setAcc_y(y);
-        setAcc_z(z);
+    public List<float[]> getAcc_tot(){
+        return acc_Features;
     }
 
     public long getTimestamp() {
