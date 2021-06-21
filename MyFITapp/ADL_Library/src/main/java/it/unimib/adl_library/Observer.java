15ad2c4e3d9aea2ac21f81c63160930ea9a837ac
GioRigamonti <76151712+GioRigamonti@users.noreply.github.com>
2021-06-21 16:18:31 +0200

    update

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/Observer.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/Observer.java
@@ -1,4 +1,4 @@
-package it.unimib.adl_library;
+/*package it.unimib.adl_library;
 
 import android.app.Service;
 import android.content.Context;
@@ -14,9 +14,9 @@ import androidx.annotation.Nullable;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
+import java.util.Map;*/
 
-public class Observer extends Service implements ADLObserver{
+/*public class Observer extends Service implements ADLObserver{
     private ADLInstance instance;
     private TFRecognizer recognizer;
     protected SensorManager mSensorManager;
@@ -98,4 +98,72 @@ public class Observer extends Service implements ADLObserver{
         Toast.makeText(context1, "Service Destroyed", Toast.LENGTH_LONG).show();
         stopReadingAccelerometer();
     }
+}*/
+
+package it.unimib.adl_library;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+public class Observer  implements ADLObserver{
+    private ADLInstance instance;
+    private TFRecognizer recognizer;
+    private ADLListener listener;
+
+    public Observer(Context context, String index) throws Exception {
+        Toast.makeText(context.getApplicationContext(), "inObserver constr", Toast.LENGTH_LONG).show();
+        instance = new ADLInstance();
+        switch (index.toUpperCase()){
+            case "TF":
+                recognizer = new TFRecognizer(context, instance);
+                break;
+            //default:
+                //throw new Exception();
+        }
+        listener = recognizer.getAccListener();
+    }
+
+    @Override
+    public String activityIdentified() {
+        return instance.getActivity();
+    }
+
+    @Override
+    public Map<String, Float> activityConfidence() {
+        return instance.getMap();
+    }
+
+    public Intent probability(){
+        Intent intent = new Intent();
+        intent.setAction("it.unimib.adl_library");
+        intent.putExtra("label", activityIdentified());
+        intent.putExtra("confidence", (Serializable) activityConfidence());
+        return intent;
+    }
+
+    public void startReadingAccelerometer(SensorManager mSensorManager) {
+        listener.clearFeatures(); //si assicura che l'istanza non ha letture precedenti
+        //ACCELEROMETRO CON GRAVITA'
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            List<Sensor> ls = mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+            for (int i = 0; i < ls.size(); i++) {
+                Sensor s_i = ls.get(i);
+                mSensorManager.registerListener(listener, s_i, SensorManager.SENSOR_DELAY_GAME);
+            }
+        }
+    }
+
+    public void stopReadingAccelerometer(SensorManager mSensorManager) {
+        mSensorManager.unregisterListener(listener);
+        listener.clearFeatures(); //pulizia delle letture precedenti
+    }
+
 }
+
