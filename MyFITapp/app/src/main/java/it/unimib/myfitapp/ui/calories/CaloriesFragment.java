e571b6b89e8f1103b9581f0b9e2cfc58bbb25465
j-maggioni <jessicamaggioni99@gmail.com>
2021-06-23 17:42:06 +0200

    Update CaloriesFragment.java

--- MyFITapp/app/src/main/java/it/unimib/myfitapp/ui/calories/CaloriesFragment.java
+++ MyFITapp/app/src/main/java/it/unimib/myfitapp/ui/calories/CaloriesFragment.java
@@ -1,14 +1,20 @@
 package it.unimib.myfitapp.ui.calories;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -23,27 +29,32 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import it.unimib.myfitapp.ProfileActivity;
 import it.unimib.myfitapp.R;
+import it.unimib.myfitapp.UserInformation;
 
 public class CaloriesFragment extends Fragment {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
     PieChart pieChart;
     PieData pieData;
     List<PieEntry> pieEntryList = new ArrayList<>();
-
-    LineChart lineChart;
-    LineData lineData;
-    ArrayList lineEntries;
-
-    private CaloriesViewModel notificationsViewModel;
+    Button addCalories;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(CaloriesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_calories, container, false);
         /*final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -52,13 +63,49 @@ public class CaloriesFragment extends Fragment {
                 textView.setText(s);
             }
         });*/
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance("https://myfitapp-a5b2b-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+        firebaseDatabase = FirebaseDatabase.getInstance();
 
+        //addCalories = (Button) root.findViewById(R.id.button_add_calories);
         drawPieChart(root);
 
 
         return root;
     }
 
+    public void buttonClickedAddCalories(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.layout_custum_dialog_add_calories, null);
+        final EditText etCalFood = alertLayout.findViewById(R.id.et_calFromFood);
+        final EditText etCalPer100 = alertLayout.findViewById(R.id.et_calPer100);
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity().getApplicationContext());
+        alert.setTitle(getResources().getString(R.string.add_calories_consumed));
+        // this is set the view from XML inside AlertDialog
+        alert.setView(alertLayout);
+        // disallow cancel of AlertDialog on click of back button and outside touch
+        alert.setCancelable(false);
+        alert.setNegativeButton(getResources().getString(R.string.annulla), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alert.setPositiveButton(getResources().getString(R.string.ok_confirm), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String foodName = etCalFood.getText().toString().trim();
+                int cal = Integer.parseInt(etCalPer100.getText().toString().trim());
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                databaseReference.child(user.getUid()).child("calories").setValue(foodName);
+                databaseReference.child(user.getUid()).child("calories").child(foodName).setValue(cal);
+                etCalFood.onEditorAction(EditorInfo.IME_ACTION_DONE);
+                etCalPer100.onEditorAction(EditorInfo.IME_ACTION_DONE);
+            }
+        });
+        AlertDialog dialog = alert.create();
+        dialog.show();
+
+    }
 
     public void drawPieChart(View v) {
         pieChart = (PieChart) v.findViewById(R.id.pieChart_calories);
