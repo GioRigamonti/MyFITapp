acad7b9efc3b4a544a448e041defa9e8bd8d0355
j-maggioni <jessicamaggioni99@gmail.com>
2021-06-28 09:47:34 +0200

    Update ADLManager.java

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLManager.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLManager.java
@@ -7,15 +7,11 @@ import java.util.Map;
 
 public abstract class ADLManager {
     protected Context context;
-    //protected SensorManager mSensorManager;
 
     public ADLManager(Context context) {
         this.context = context;
-        //mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
     }
 
-    //public abstract void startReadingAccelerometer();
-    //public abstract void stopReadingAccelerometer();
     public abstract ADLListener getAccListener();
     public abstract void doInference(ADLInstance instance)throws Exception;
     protected abstract String setLabel();
