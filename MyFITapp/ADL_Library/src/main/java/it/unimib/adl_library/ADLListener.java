4463094a80f1a86f600d0c5b1b026bf2bce0e296
j-maggioni <jessicamaggioni99@gmail.com>
2021-06-28 09:47:24 +0200

    Update ADLListener.java

--- MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLListener.java
+++ MyFITapp/ADL_Library/src/main/java/it/unimib/adl_library/ADLListener.java
@@ -24,20 +24,7 @@ public class ADLListener implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        /*Sensor sensor = event.sensor;
-        //ACCELEROMETRO CON GRAVITA'
-        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            adl_instance.setAccFeatures(event.values[0], event.values[1], event.values[2]);
-            int rilevazioniAcc = adl_instance.getAccFeatures().size();
-
-            if (rilevazioniAcc >= SAMPLE_PER_SEC && rilevazioniAcc % SAMPLE_PER_SEC == 0) {  //lista ha dim_finale = 150, si sono letti 50 campioni/sec
-                //adl_instance.setFrame(rilevazioniAcc);   //crea finestre di lettura da 150 campioni
-                try {
-                    recognizer.doInference(adl_instance);
-                } catch (Exception e) {}
-            }
-        }*/
-        if (adl_instance.getAccFeatures().size() < SAMPLE_PER_SEC) {   //lista ha dim_finale = 50, si sono letti 50 campioni/sec
+        if (adl_instance.getAccFeatures().size() < SAMPLE_PER_SEC) {
             Sensor sensor = event.sensor;
             //ACCELEROMETRO CON GRAVITA'
             if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
@@ -55,11 +42,8 @@ public class ADLListener implements SensorEventListener {
     public void onAccuracyChanged(Sensor sensor, int accuracy){}
 
     public void clearFeatures() {
-        //List<float[]>  clearFeatures = adl_instance.getAccFeatures().subList(0,adl_instance.FRAME - adl_instance.OVERLAP);
-        //List<float[]>  newFeatures = adl_instance.getAccFeatures().subList(adl_instance.FRAME - adl_instance.OVERLAP + 1, adl_instance.getAccFeatures().size());
         adl_instance.getAccFeatures().subList(0,adl_instance.FRAME - adl_instance.OVERLAP).clear();
         adl_instance.acc_Features.subList(0,adl_instance.getAccFeatures().size());
-
     }
 
 }
